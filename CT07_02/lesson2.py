ab13d22b83ef3138e2cfec69d492b56f0b15e548
LL77LL75 <lancelimenze13@gmail.com>
2025-01-13 10:16:58 +0000

    Jan 13, 2025, 10:16 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -4,17 +4,17 @@ answer1 = int(input("how many legs does an octopus have? "))
 # while 1>0:
 #     a = a+1
 #     print(a)
-while answer1 != 8:
+while (answer1 != 8) or not(str(answer1) == "skip"):
     answer1 = int(input("how many legs does an octopus have? "))
 else:
     SCORe+=1
     answer2 = int(input("how many months are there in a year? "))
-    while (answer2 != 12):
+    while (answer2 != 12) or not(str(answer2) == "skip"):
         answer2 = int(input("how many months are there in a year? "))
     else:
         SCORe+=1
         answer3 = int(input("how letters are there in the alpabet? "))
-        while answer3 != 26:
+        while (answer3 != 26) or not(str(answer2) == "skip"):
             answer3 = int(input("how letters are there in the alpabet? "))
 SCORe+=1
 print("u have at least 1 iq")
