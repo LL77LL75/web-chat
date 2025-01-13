e89125d35afee4323aaa70fa249a6a89a5ef1819
LL77LL75 <lancelimenze13@gmail.com>
2025-01-13 10:45:25 +0000

    Jan 13, 2025, 10:45 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,29 +1,29 @@
-if 1 == 3:
-    SCORe = 0
-    answer1 = input("how many legs does an octopus have? ")
-    a = 1
-    # while 1>0:
-    #     a = a+1
-    #     print(a)
-    while (answer1 != "8") and not(answer1 == "skip"):
-        answer1 = input("how many legs does an octopus have? ")
-    else:
-        if answer1 != "skip":
-            SCORe+=1
-        answer2 = input("how many months are there in a year? ")
-        while (answer2 != "12") and not(answer2 == "skip"):
-            answer2 = input("how many months are there in a year? ")
-        else:
-            if answer2 != "skip":
-                SCORe+=1
-            answer3 = input("how letters are there in the alpabet? ")
-            while (answer3 != "26") and not(answer3 == "skip"):
-                answer3 = input("how letters are there in the alpabet? ")
-    if answer3 != "skip":
-        SCORe+=1
-    print("u have at least 1 iq")
-    print("your score is " + str(SCORe))
-    # answer1 = input("what comes once in a minute twice in a moment but never in a thousand years? ")
-    # while answer1 !="the letter m":
-    #     answer1 = input("what comes once in a minute twice in a moment but never in a thousand years? ")
-import time
+# if 1 == 3:
+#     SCORe = 0
+#     answer1 = input("how many legs does an octopus have? ")
+#     a = 1
+#     # while 1>0:
+#     #     a = a+1
+#     #     print(a)
+#     while (answer1 != "8") and not(answer1 == "skip"):
+#         answer1 = input("how many legs does an octopus have? ")
+#     else:
+#         if answer1 != "skip":
+#             SCORe+=1
+#         answer2 = input("how many months are there in a year? ")
+#         while (answer2 != "12") and not(answer2 == "skip"):
+#             answer2 = input("how many months are there in a year? ")
+#         else:
+#             if answer2 != "skip":
+#                 SCORe+=1
+#             answer3 = input("how letters are there in the alpabet? ")
+#             while (answer3 != "26") and not(answer3 == "skip"):
+#                 answer3 = input("how letters are there in the alpabet? ")
+#     if answer3 != "skip":
+#         SCORe+=1
+#     print("u have at least 1 iq")
+#     print("your score is " + str(SCORe))
+#     # answer1 = input("what comes once in a minute twice in a moment but never in a thousand years? ")
+#     # while answer1 !="the letter m":
+#     #     answer1 = input("what comes once in a minute twice in a moment but never in a thousand years? ")
+# import time
