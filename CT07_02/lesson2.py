3d70e24a00250c10a6bb8d45cf2cc8789ef95611
LL77LL75 <lancelimenze13@gmail.com>
2025-01-13 10:07:12 +0000

    Jan 13, 2025, 10:07 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,12 +1,15 @@
-answer3 = ""
-answer1 = int(input("how many legs does an octopus have?"))
+answer1 = int(input("how many legs does an octopus have? "))
 # a = 1
 # while 0>1:
 #     a+=1
 #     print(a)
 while answer1 != 8:
-    answer = int(input("how many legs does an octopus have?"))
+    answer = int(input("how many legs does an octopus have? "))
 else:
-    answer2 = int(input("how many months are there in a year?"))
+    answer2 = int(input("how many months are there in a year? "))
     while answer2 != 12:
-        
+        answer2 = int(input("how many months are there in a year? "))
+    else:
+        answer3 = int(input("how letters are there in the alpabet? "))
+        while answer3 != 26:
+            answer3 = int(input("how letters are there in the alpabet? "))
