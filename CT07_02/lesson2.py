12c4b8eaba694374c2bcdf7c7dde62949ae60a79
LL77LL75 <lancelimenze13@gmail.com>
2025-01-13 10:48:13 +0000

    Jan 13, 2025, 10:48 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -28,4 +28,4 @@
 #     #     answer1 = input("what comes once in a minute twice in a moment but never in a thousand years? ")
 import time
 study = int(input("how many minutes do you want to study"))
-time.sleep(study)
+time.sleep(study**60)
