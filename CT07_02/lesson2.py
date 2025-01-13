d245300bd066705436a118eb1b25034b10c1530b
LL77LL75 <lancelimenze13@gmail.com>
2025-01-13 09:58:24 +0000

    Jan 13, 2025, 9:58 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,6 +1,7 @@
-a = 1
-while 0>1:
-    a+=1
-    print(a)
-while
-input("how many legs does an octopus have?")
+answer = int(input("how many legs does an octopus have?"))
+# a = 1
+# while 0>1:
+#     a+=1
+#     print(a)
+while answer != 8:
+    answer = int(input("how many legs does an octopus have?"))    
