8a5323902b4fd5cf6d7befdeacd1a9350366b159
LL77LL75 <lancelimenze13@gmail.com>
2025-01-20 09:44:57 +0000

    Jan 20, 2025, 9:44 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -2,8 +2,11 @@
 # while True:
 #     print(i)
 #     i+=1
-savings = 0
-while savings <100:
-    savings += int(input("how much did you save today? "))
-else:
-    print("you saved $100 or more!")
+# savings = 0
+# while savings <100:
+#     savings += int(input("how much did you save today? "))
+# else:
+#     print("you saved $100 or more!")
+lives = 3
+questions = 
+while (questions<1)
