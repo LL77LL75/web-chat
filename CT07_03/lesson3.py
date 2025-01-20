d08c6c733e5905554d4ceb6214c4175bc5c252e4
LL77LL75 <lancelimenze13@gmail.com>
2025-01-20 09:41:59 +0000

    Jan 20, 2025, 9:41 AM

--- CT07_03/lesson3.py
+++ CT07_03/lesson3.py
@@ -1,2 +1,9 @@
-print("Hello from lesson 3")
-
+# i = 1
+# while True:
+#     print(i)
+#     i+=1
+savings = 0
+while savings <100:
+    savings += int(input("how much did you save today? "))
+else:
+    print("you saved $100 or more!")
