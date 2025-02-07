37bdf9582f358805676740b1fadc0e6872a1c098
LL77LL75 <lancelimenze13@gmail.com>
2025-02-07 10:22:27 +0000

    Feb 7, 2025, 10:22 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -1,10 +1,10 @@
-a = 11
-import time
-while a >1:
-    time.sleep(1)
-    a-=1
-    print(a)
-print("happy New year!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+# a = 11
+# import time
+# while a >1:
+#     time.sleep(1)
+#     a-=1
+#     print(a)
+# print("happy New year!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
 ## Task 3: Flight Round the Globe
 # Task: Write a program to keep track of the countries you
@@ -18,3 +18,7 @@ print("happy New year!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 #    "I would like to visit Germany"
 #    "I would like to visit Japan"
 #    ... 
+goto = []
+answer = input("where u want to go ")
+while answer != "end":
+    goto.append
