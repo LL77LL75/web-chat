a09a39a6a520cba8ed3a12764ad5fab8aba74dec
LL77LL75 <lancelimenze13@gmail.com>
2025-02-07 09:56:36 +0000

    Feb 7, 2025, 9:56 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -1,7 +1,7 @@
-a = 10
+a = 11
 import time
 for i in range(10):
     time.sleep(1)
     a-=1
     print(a)
-print("happy")
+print("happy New year!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
