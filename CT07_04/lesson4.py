916cd166dc31407f911a51037ec178293b367663
LL77LL75 <lancelimenze13@gmail.com>
2025-02-07 10:53:19 +0000

    Feb 7, 2025, 10:53 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -48,4 +48,5 @@
 want = [pizza,]
 answer = input("wat u want to eat")
 while answer != "end" and answer in want:
-    print("We sell that.Now take a seat in the definitely")
+    print("We sell that.Now take a seat in the definitely not eletric chair")
+    
