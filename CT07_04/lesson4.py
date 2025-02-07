6f35552b2bed16be6d8fde9610f95d696d88a5a8
LL77LL75 <lancelimenze13@gmail.com>
2025-02-07 10:18:39 +0000

    Feb 7, 2025, 10:18 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -5,3 +5,16 @@ while a >1:
     a-=1
     print(a)
 print("happy New year!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+## Task 3: Flight Round the Globe
+# Task: Write a program to keep track of the countries you
+# are visiting.
+
+# 1. Use a while loop to ask the user what country they
+#    would like to visit.
+# 2. Add the country into a list
+# 3. If the user types "end", exit the loop
+# 4. Print all the countries in the list in this format.
+#    "I would like to visit Germany"
+#    "I would like to visit Japan"
+#    ... 
