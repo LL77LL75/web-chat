94d323033ba0f704cc8ee028f78997621b56e859
LL77LL75 <lancelimenze13@gmail.com>
2025-02-07 10:07:52 +0000

    Feb 7, 2025, 10:07 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -5,3 +5,10 @@ while a >1:
     a-=1
     print(a)
 print("happy New year!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+# ## Task 1: List of planets
+# **Task: Create a list of 8 planets in the solar system**
+
+# **Task 1a**:
+# Create a list of 8 planets in the solar system.
+# (Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune)
