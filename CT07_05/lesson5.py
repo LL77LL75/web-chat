b7dc7aa5ce8db2786c1e40e4489fd54687d2560a
LL77LL75 <lancelimenze13@gmail.com>
2025-02-14 10:43:07 +0000

    Feb 14, 2025, 10:43 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -85,4 +85,4 @@ heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
 # # Hint:
 # # use .index("value of something in the list") to find the index
 # # of an item
-print(namelist[heightlist.index[max]])
+print(namelist[heightlist.index[max(heightlist)]])
