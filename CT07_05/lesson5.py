fffb559753785aef31e844672087ec2070a42ba3
LL77LL75 <lancelimenze13@gmail.com>
2025-02-14 09:58:14 +0000

    Feb 14, 2025, 9:58 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -8,7 +8,7 @@ foods = ["pizza","chips","ice cream","banana"]
 # **Recap 1b**:
 # You no longer like to eat the 3rd item on your list,
 # delete it
-foods.pop(3)
+foods.pop(2)
 # **Recap 1c**:
 # Add 1 more item into your list
 foods.append("poop")
