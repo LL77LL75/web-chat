3e76246483800cd21149e3d0c3e696e7ecd01a26
LL77LL75 <lancelimenze13@gmail.com>
2025-02-14 10:41:12 +0000

    Feb 14, 2025, 10:41 AM

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,87 +1,87 @@
-# Lesson 5 - List Variables II
+# # Lesson 5 - List Variables II
 
-## Recap 1: Favourite Food List
-# **Recap 1a**:
-# Create a list of 5 foods that you like to eat
-# foods = ["pizza","chips","ice cream","banana"]
+# ## Recap 1: Favourite Food List
+# # **Recap 1a**:
+# # Create a list of 5 foods that you like to eat
+# # foods = ["pizza","chips","ice cream","banana"]
 
-# **Recap 1b**:
-# You no longer like to eat the 3rd item on your list,
-# delete it
-# foods.pop(2)
-# **Recap 1c**:
-# Add 1 more item into your list
-# foods.append("poop")
-# **Recap 1d**:
-# Write a for loop to say all the food items in your list
-# for i in range(len(foods)):
-#     print(foods[i])
+# # **Recap 1b**:
+# # You no longer like to eat the 3rd item on your list,
+# # delete it
+# # foods.pop(2)
+# # **Recap 1c**:
+# # Add 1 more item into your list
+# # foods.append("poop")
+# # **Recap 1d**:
+# # Write a for loop to say all the food items in your list
+# # for i in range(len(foods)):
+# #     print(foods[i])
 
-# for food in foods:
-#     print(food)
+# # for food in foods:
+# #     print(food)
 
-## Task 1: List of 100 numbers 
-# You are preparing for an upcoming lucky draw session at your
-# school. You have been tasked to create a program that will pick
-# 100 lucky winners.
+# ## Task 1: List of 100 numbers 
+# # You are preparing for an upcoming lucky draw session at your
+# # school. You have been tasked to create a program that will pick
+# # 100 lucky winners.
+# # import random
+# # By importing the 'random' library and using 'random.randint()',
+# # create a program to create 100 random numbers in a list
+# # 1. Use a loop to add 100 random numbers into your list.
+# # 2. Each number added range between 1 to 1000
 # import random
-# By importing the 'random' library and using 'random.randint()',
-# create a program to create 100 random numbers in a list
-# 1. Use a loop to add 100 random numbers into your list.
-# 2. Each number added range between 1 to 1000
-import random
-b = []
-randint = random.randint(1,1000)
-while len(b) < 101:
-    if randint not in b:
-        b.append(randint)
-    randint = random.randint(1,1000)
-print(b)
+# b = []
+# randint = random.randint(1,1000)
+# while len(b) < 101:
+#     if randint not in b:
+#         b.append(randint)
+#     randint = random.randint(1,1000)
+# print(b)
 
-## Task 2: List of 100 unique numbers
-# The program you have created from the previous task will
-# sometimes generate duplicate numbers. Modify your program so
-# that the 100 numbers generated are all unique.
+# ## Task 2: List of 100 unique numbers
+# # The program you have created from the previous task will
+# # sometimes generate duplicate numbers. Modify your program so
+# # that the 100 numbers generated are all unique.
 
-# Modify your program from the previous task to create 100 random
-# unique numbers in a list.
-# 1. Use a loop to add 100 random numbers into your list.
-# 2. Each number added range between 1 to 1000
-# 3. Ensure that all the numbers are unique
-# 4. Print the list of 100 unique numbers created
+# # Modify your program from the previous task to create 100 random
+# # unique numbers in a list.
+# # 1. Use a loop to add 100 random numbers into your list.
+# # 2. Each number added range between 1 to 1000
+# # 3. Ensure that all the numbers are unique
+# # 4. Print the list of 100 unique numbers created
 
-## Task 3: Score Taker
-# Imagine the list that you have created in Task 2 represent the
-# score of a 100 students.
+# ## Task 3: Score Taker
+# # Imagine the list that you have created in Task 2 represent the
+# # score of a 100 students.
 
-# Find the maximum, minimum and average from the list.
+# # Find the maximum, minimum and average from the list.
 
-# 1. Using the 'max()' function, find the maximum score.
-# 2. Using the 'min()' function, find the minimum score.
-# 3. Using the 'sum()' and 'len()' function, calculate the
-#    average score.
+# # 1. Using the 'max()' function, find the maximum score.
+# # 2. Using the 'min()' function, find the minimum score.
+# # 3. Using the 'sum()' and 'len()' function, calculate the
+# #    average score.
 
-# print(max(b))
-# print(min(b))
-# print(sum(b)/len(b))
+# # print(max(b))
+# # print(min(b))
+# # print(sum(b)/len(b))
 
-## Task 4: Who is the tallest?
-# Task: You are given 2 lists, 
-# **namelist** contains a list of students in your class
-# **heightlist** contains a list of the corresponding student's
-#                height
+# ## Task 4: Who is the tallest?
+# # Task: You are given 2 lists, 
+# # **namelist** contains a list of students in your class
+# # **heightlist** contains a list of the corresponding student's
+# #                height
 
-# 1. Determine who is the tallest in class, and what is his/ her
-#    name
-# 2. Determine who is the shortest in class, and what is his/ her
-#    name
+# # 1. Determine who is the tallest in class, and what is his/ her
+# #    name
+# # 2. Determine who is the shortest in class, and what is his/ her
+# #    name
 
-# Sample Data (Copy + paste the below code):
-# namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
-#             "Sophia", "Lucas", "Mia", "Aiden"
-#             ]
-# heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
+# # Sample Data (Copy + paste the below code):
+namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
+            "Sophia", "Lucas", "Mia", "Aiden"
+            ]
+heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
 
-# Hint:
-# use .index("value of something in the list") to find the index
-# of an item
+# # Hint:
+# # use .index("value of something in the list") to find the index
+# # of an item
