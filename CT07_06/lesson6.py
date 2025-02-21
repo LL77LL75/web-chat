46c631edb9382414caca2f5bf66c1d5983ef43fd
LL77LL75 <lancelimenze13@gmail.com>
2025-02-21 10:02:31 +0000

    Feb 21, 2025, 10:02 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -30,4 +30,5 @@ print(winners)
 # 5. Using index(), print out the index of the printed random
 #    number.
 print("the max is " + str(max(winners)))
-print("the min is " + str(max(winners)))
+print("the min is " + str(min(winners)))
+print("the max is " + str(max(winners)))
