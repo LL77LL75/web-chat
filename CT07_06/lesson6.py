d9ed844797f39b70175920624afe4357d736e6f3
LL77LL75 <lancelimenze13@gmail.com>
2025-02-21 10:39:41 +0000

    Feb 21, 2025, 10:39 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -78,7 +78,7 @@ students = [
 #     print(str(student[0]) +","+str(student[1]))
 
 for student in students:
-    name, gender, = student
+    name, gender,_ = student
     print(name,gender)
 ### the above is a nested list. Study and discuss it before we
 ### move on.
