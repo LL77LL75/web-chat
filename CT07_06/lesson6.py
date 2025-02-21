79f14951e80687f1414174d56b4d01be0d1d9920
LL77LL75 <lancelimenze13@gmail.com>
2025-02-21 10:51:57 +0000

    Feb 21, 2025, 10:51 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -100,6 +100,8 @@ for student in students:
 # 3. Write a for loop and name all the boys
 # 4. Write a for loop and name all the girls
 # 5. Print out how many boys and girls there are
+for students in students:
+    _,
 boys = []
 girls = []
 for s in students:
