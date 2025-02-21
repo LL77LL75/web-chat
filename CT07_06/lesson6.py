dcd23ac2fb640c62e57551b44abbbaee49e87f22
LL77LL75 <lancelimenze13@gmail.com>
2025-02-21 10:07:01 +0000

    Feb 21, 2025, 10:07 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -12,8 +12,8 @@
 # 3. Ensure that all the numbers are unique
 import random
 winners = []
-while len(winners) < 100:
-    item = random.randint(1,1000)
+while len(winners) < 10000:
+    item = random.randint(1,1000000)
     if item not in winners:
         winners.append(item)
 print(winners)
