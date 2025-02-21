fa5a49b28e0fb4931c9f8936402bb95f2a8ff84d
LL77LL75 <lancelimenze13@gmail.com>
2025-02-21 10:38:07 +0000

    Feb 21, 2025, 10:38 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -78,11 +78,11 @@ students = [
 #     print(str(student[0]) +","+str(student[1]))
 
 for student in students:
-    name, gender = student
+    name, gender,  = student
     print(name,gender)
 ### the above is a nested list. Study and discuss it before we
 ### move on.
-
+e
 # 1. Write a for loop to print out the names of each student and
 #    the gender beside.
    
