156122debd698f84e91276f375bd8ac2fdbfbe1d
LL77LL75 <lancelimenze13@gmail.com>
2025-02-21 09:39:23 +0000

    Feb 21, 2025, 9:39 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,2 +1,12 @@
-print("Hello from lesson 6")
+# Lesson 6 - 2-dimensional list
 
+## Recap 1: List of 100 unique numbers
+# **Recap 1a**:
+# You are preparing for an upcoming lucky draw session at your
+# school. However, there must be no repeating winning numbers.
+
+# Task: Create a program to create 100 random unique numbers in
+# a list
+# 1. Use a loop to add 100 random numbers into your list.
+# 2. Each number added range between 1 to 1000
+# 3. Ensure that all the numbers are unique
