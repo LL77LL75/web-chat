5f79357299ebebae9fca4fb9d73faa295afd4d13
LL77LL75 <lancelimenze13@gmail.com>
2025-02-21 09:50:24 +0000

    Feb 21, 2025, 9:50 AM

--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -16,7 +16,6 @@ item = random.randint(1,100)
 while len(winners) < 101:
     if item not in winners:
         winners.append(item)
-        item = random.randint(1,100)
 print(winners)
 print(len(winners))
 
