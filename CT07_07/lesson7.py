89a0895b2cb6f8aaf058fc1a0fc5652f46837baa
LL77LL75 <lancelimenze13@gmail.com>
2025-02-28 09:41:14 +0000

    Feb 28, 2025, 9:41 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -1,2 +1,22 @@
-print("Hello from lesson 7")
+# Lesson 7 - 2 dimensional list
 
+## Recap 1: Student Contact Database
+# Task: Create a contact database for students
+# 1. Create a list variable called students
+# 2. Create 3 lists called student1, student2, student3
+#     each student should have the following info:
+#         1. name
+#         2. phone number
+#         3. CCA
+# 3. Add student1, student2, student3 into the students list.
+# 4. Unpack the list and use loop and string concatenation to
+#    print the details for each student in the following format:
+
+#    Name: James
+#    Phone number: 85726845
+#    CCA: Hockey
+student=[]
+# students = []
+# student1 = ["John", 98453126, "Hockey"]
+# student2 = ["Adam", 93029102, "Soccer"]
+# student3 = ["Sylvia", 87894032, "Dance"]
