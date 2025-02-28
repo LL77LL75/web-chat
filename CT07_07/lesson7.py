10b0649b2a5428c568ca6465207e610445652091
LL77LL75 <lancelimenze13@gmail.com>
2025-02-28 09:45:10 +0000

    Feb 28, 2025, 9:45 AM

--- CT07_07/lesson7.py
+++ CT07_07/lesson7.py
@@ -21,7 +21,7 @@ student2 = ["Adam", 93029102, "Soccer"]
 student3 = ["Sylvia", 87894032, "Dance"]
 students = [student1,student2,student3]
 for student in students:
-    name,phone_number,
+    name,phone_number,CCA = student
 for student in students:
     for s in student: 
         print
