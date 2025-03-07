6dbe9061e96775f92e54b07e2d677205236afd91
LL77LL75 <lancelimenze13@gmail.com>
2025-03-07 10:07:07 +0000

    Mar 7, 2025, 10:07 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -11,14 +11,14 @@
 # #prints the order
 # for i in order:
 #     print(str(order.index(i)) + "." + i)
-import random
-hp = 100
-battles = 0
-# sets the hero's health points
-while hp >0:
-    print("after fighting monsters, his health is now: " + str(hp))
-    # reduces the hero's health
-    hp = hp - random.randint(1,5)
-    #checks if the hero has more than 0 health
-    battles += 1
-print("the hero died after fighting " + str(battles) + " battles")
+# import random
+# hp = 100
+# battles = 0
+# # sets the hero's health points
+# while hp >0:
+#     print("after fighting monsters, his health is now: " + str(hp))
+#     # reduces the hero's health
+#     hp = hp - random.randint(1,5)
+#     #checks if the hero has more than 0 health
+#     battles += 1
+# print("the hero died after fighting " + str(battles) + " battles")
