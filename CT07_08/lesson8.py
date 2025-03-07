218f0f72c1bcc3663ff0286dc935162162a1c001
LL77LL75 <lancelimenze13@gmail.com>
2025-03-07 10:07:38 +0000

    Mar 7, 2025, 10:07 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -10,7 +10,7 @@ while ans != "end":
 print("you have ordered the following:")
 #prints the order
 for i in order:
-    print(str(order.index(i)) + "." + i)
+    print(str(order.index(i+1)) + "." + i)
 # import random
 # hp = 100
 # battles = 0
