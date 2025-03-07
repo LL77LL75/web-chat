ae27eac65a43326868a1616e54d504b4ebf9a458
LL77LL75 <lancelimenze13@gmail.com>
2025-03-07 10:07:17 +0000

    Mar 7, 2025, 10:07 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,16 +1,16 @@
-# ans = input("what would you like to order? ")
-# #asks the user what he/she wants to order
-# order = []
-# #allows the orders to be stored
-# while ans != "end":
-#     order.append(ans)
-#     #adds the order
-#     ans = input("what would you like to order? ")
-#     #changes the answer
-# print("you have ordered the following:")
-# #prints the order
-# for i in order:
-#     print(str(order.index(i)) + "." + i)
+ans = input("what would you like to order? ")
+#asks the user what he/she wants to order
+order = []
+#allows the orders to be stored
+while ans != "end":
+    order.append(ans)
+    #adds the order
+    ans = input("what would you like to order? ")
+    #changes the answer
+print("you have ordered the following:")
+#prints the order
+for i in order:
+    print(str(order.index(i)) + "." + i)
 # import random
 # hp = 100
 # battles = 0
