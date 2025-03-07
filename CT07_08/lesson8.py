a4f0efbd515db9dd06edda6170cae896f9ce2c9d
LL77LL75 <lancelimenze13@gmail.com>
2025-03-07 09:47:25 +0000

    Mar 7, 2025, 9:47 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -6,6 +6,6 @@ while hp >0:
     #checks if the hero has more than 0 health
     hp = hp - random.randint(1,5)
     # reduces the hero's health
-    h
+    battles += 1
     print("after fighting monsters the hero has " + str(hp) + " left")
 print("the hero")
