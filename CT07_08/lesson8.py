abf3c2ab10b6446b9d684304287e28640725a0b6
LL77LL75 <lancelimenze13@gmail.com>
2025-03-07 09:48:53 +0000

    Mar 7, 2025, 9:48 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -4,8 +4,8 @@ battles = 0
 # sets the hero's health points
 while hp >0:
     print("after fighting monsters the hero has " + str(hp) + "health left")
-    #checks if the hero has more than 0 health
-    hp = hp - random.randint(1,5)
     # reduces the hero's health
+    hp = hp - random.randint(1,5)
+    #checks if the hero has more than 0 health
     battles += 1
 print("the hero died after fighting " + str(battles) + " battles")
