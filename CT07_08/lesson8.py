eab0d3ed28be3fa26ce9fe715c39b7c0405cc7bb
LL77LL75 <lancelimenze13@gmail.com>
2025-03-07 09:51:52 +0000

    Mar 7, 2025, 9:51 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,11 +1,6 @@
-import random
-hp = 100
-battles = 0
-# sets the hero's health points
-while hp >0:
-    print("after fighting monsters the hero has " + str(hp) + "health left")
-    # reduces the hero's health
-    hp = hp - random.randint(1,5)
-    #checks if the hero has more than 0 health
-    battles += 1
-print("the hero died after fighting " + str(battles) + " battles")
+ans = input("what would you like to order? ")
+#asks the user what he/she wants to order
+order = []
+#allows the orders to be stored
+while ans != "end":
+    ans.append
