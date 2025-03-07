45e3d30f1a2d71829bb3eaafaca1b09536295991
LL77LL75 <lancelimenze13@gmail.com>
2025-03-07 09:46:45 +0000

    Mar 7, 2025, 9:46 AM

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,9 +1,11 @@
 import random
 hp = 100
+battles = 0
 # sets the hero's health points
 while hp >0:
     #checks if the hero has more than 0 health
     hp = hp - random.randint(1,5)
     # reduces the hero's health
+    h
     print("after fighting monsters the hero has " + str(hp) + " left")
 print("the hero")
