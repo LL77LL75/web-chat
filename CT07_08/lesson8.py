e3e6b1866cb8eec74e2b5d1fe0f024a133411fec
LL77LL75 <lancelimenze13@gmail.com>
2025-03-21 10:54:34 +0000

    Co-authored-by: Amos Tan <amos.tan@computhink.com.sg>

--- CT07_08/lesson8.py
+++ CT07_08/lesson8.py
@@ -1,26 +1,88 @@
-ans = input("what would you like to order? ")
-#asks the user what he/she wants to order
-order = []
-#allows the orders to be stored
-while ans != "end":
-    order.append(ans)
-    #adds the order
-    ans = input("what would you like to order? ")
-    #changes the answer
-print("you have ordered the following:")
-#prints the order
-order1 = 1
-for i in order:
-    print(str(order1) + "." + i)
-    order1 += 1
-# import random
-# hp = 100
-# battles = 0
-# # sets the hero's health points
-# while hp >0:
-#     print("after fighting monsters, his health is now: " + str(hp))
-#     # reduces the hero's health
-#     hp = hp - random.randint(1,5)
-#     #checks if the hero has more than 0 health
-#     battles += 1
-# print("the hero died after fighting " + str(battles) + " battles")
+# # Lesson 8 - String splitting, list joining, and
+# #            finding substring
+
+# ## Recap 1: List Manipulation
+# Given 3 lists, merge them into a single list, remove
+# duplicates, and then split the list into 2 halves,
+# ensuring both halves are sorted.
+
+# list1 = [3, 2, 1]
+# list2 = [6, 5, 5]
+# list3 = [9, 8, 7]
+
+# 1. Merge the 3 lists and remove duplicates.
+# 2. Sort the resulting list.
+# 3. Split the list into 2 sorted halves.
+# 4. Print the halves.
+# 5. 
+# pw = input("password ")
+# if len(pw)>8:
+#     if (pw.isupper() and pw.islower()):
+#         print("password not accepted")
+#     else :
+#         if (pw.isalpha() and pw.isdigit()) :
+#             print("password not accepted")
+#         else:
+#             print(" your password is secure")
+# word = input("words")
+# letters = ""
+# count = 1
+# for i in word:
+#     if count % 2 == 0:
+#         letters += i.upper()
+#         count+=1
+#     else:
+#         letters += i.lower()
+#         count+=1
+# print(letters)
+# list =  ['Computers', 'empower', 'our', 'modern', 'world', 'with', 'their', 'digital', 'brains.']
+# word = ",".join(list)
+# sentance= "Hello World"
+
+# for i in sentance:
+#     i
+
+
+
+## Task 6: Checking if a Word is a Palindrome
+# Write a program to check if the word "radar" is a palindrome.
+# A word is a palindrome if it reads the same backward as forward.
+
+# Example:
+# Input: "radar"
+# Output: True
+
+
+
+## Task 7: Checking user input for Palindrome
+# Create a while loop that will endlessly ask user for a word and
+# check if the word is a Palindrome.
+
+# The while loop will end when user says "end"
+
+# Output:
+# 1 palindrome detected: 
+# a
+# inputt = input("WOT U WANT ")
+# reversed = inputt[::-1]
+# while not (inputt == "end"):
+#     if reversed == inputt:
+#         print("it is a palindrome")
+#         inputt = input("WOT U WANT ")
+#         reversed = inputt[::-1]
+#     else:
+#         print("it is as good as a fart")
+#         inputt = input("WOT U WANT ")
+#         reversed = inputt[::-1]
+        
+
+
+
+## Task 8: Checking user input for presence of Palindrome
+# Create a program that will check if a palindrome exists in a
+# sentence.
+
+### Example:
+# Input:
+# Enter a sentence: <<"This is a sentence, check for palindrome">>
+
--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -1,2 +1,71 @@
 print("Hello from lesson 9")
 
+import turtle
+t = turtle.Turtle()
+window = turtle.Screen()
+window.setup(800,400)
+window.bgcolor("forestgreen")
+t.seth(0)
+sides = int(input("sides "))
+t.pendown()
+for i in range(sides):
+    t.forward(100)
+    t.left(360/sides)
+t.penup()
+t.sety(250)
+t.setx(-400)
+while t.xcor() < 400:
+    t.setx(t.xcor()- 25)
+    t.stamp()
+
+# **Task 1b**: Drawing the finish line
+# Adding on to your previous answer, create a line of black
+# squares at y = 250 by creating a black square turtle object
+# and stamping it from  to x = 300 at 25 pixel-
+# intervals
+
+# 1. Create a turtle object named 'pen'
+# 2. Lift the pen up
+# 3. Using '.shape()', set the turtle object's shape to "square"
+# 4. Using '.sety()', set turtle object's y position to 250
+# 5. Using a 'for i in range()' loop, use the 'i' variable to
+#    create a stamp of 'pen' turtle object from x = -300 to
+#    x = 300 at 25-pixel intervals:
+#         a. Set 'pen' turtle's x coordinate using '.setx()'
+#         b. Stamp a copy of 'pen' turtle at its current
+#            position using '.stamp()'
+
+# Tip: Use '.mainloop()' at the end of your program to keep the
+# window open
+
+# You should see this when you run the program! (Refer to slides)
+
+
+# **Task 1c**: Drawing the start line
+# Adding on to your previous answer, use the 'pen' turtle object
+# you have created earlier to draw a horizontal yellow start
+# line at y = -250.
+
+# 1. Using '.goto()', set 'pen' turtle's coordinates to
+#    (-300, -250)
+# 2. Using the following commands, set the colour of 'pen' to
+#    "yellow", set heading to 0 and move forward by 600
+#    before hiding the turtle:
+#         a. '.pencolor()'
+#         b. '.pendown()'
+#         c. '.seth()'
+#         d. '.forward()'
+#         e. '.hideturtle()'
+
+# Tip: Use '.mainloop()' at the end of your program to keep the
+# window open
+
+# You should see this when you run the program! (Refer to slides)
+
+
+
+
+
+
+
+window.mainloop()
