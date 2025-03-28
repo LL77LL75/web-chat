6d4b630b78d44afb8780f62687ad8abf8d62643c
LL77LL75 <lancelimenze13@gmail.com>
2025-03-28 10:13:54 +0000

    Mar 28, 2025, 10:13 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -109,7 +109,7 @@ bob.color('blue')
 bob.write("bob", align="center", font=('Arial', 20))
 window.mainloop()
 import random
-input("WHO WILL WIN")
+guess = input("WHO WILL WIN")
 # **Task 1f**: Input to guess the race winner
 # Adding on to your previous answer, ask the user to guess the
 # winner and store the user's response in the 'guess' variable
