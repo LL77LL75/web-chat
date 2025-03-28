4f09fd087217c69b3996af9e32974c16268d7e36
LL77LL75 <lancelimenze13@gmail.com>
2025-03-28 10:47:59 +0000

    Mar 28, 2025, 10:47 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -1,2 +1,14 @@
-print("Hello from lesson 9")
+## Task 1: Function without parameter (w/o turtle)
+# You are required to print the same "Motion Detected" phrase
+# multiple times as part of a motion detecting program that you
+# are creating.
+
+# Create an 'alert()' function that will print "Motion Detected"
+# whenever the function is called.
+
+# **Example**
+# Input:
+#     alert()
+# Output:
+#     Motion Detected
 
