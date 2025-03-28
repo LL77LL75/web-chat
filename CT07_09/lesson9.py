450aec2f3670be933d482e5f1b6a7efebd44e45c
LL77LL75 <lancelimenze13@gmail.com>
2025-03-28 09:54:55 +0000

    Mar 28, 2025, 9:54 AM

--- CT07_09/lesson9.py
+++ CT07_09/lesson9.py
@@ -35,7 +35,7 @@ sally.shape('turtle')
 sally.seth(90)
 sally.penup()
 sally.goto(-400,-250)
-
+sally.goto(0,250)
 # **Task 1b**: Drawing the finish line
 # Adding on to your previous answer, create a line of black
 # squares at y = 250 by creating a black square turtle object
