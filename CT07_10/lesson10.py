5768857859787e11bb291562f689b49658f50e0b
LL77LL75 <lancelimenze13@gmail.com>
2025-04-04 10:37:30 +0000

    Apr 4, 2025, 10:37 AM

--- CT07_10/lesson10.py
+++ CT07_10/lesson10.py
@@ -164,7 +164,7 @@ import random
 # 6. Ask the user if they want to play again.
 # 7. The loop continues until the user chooses not to play again
 moves = ["rock","paper","siscors"]
-win
+winLose=[]
 def generate_computer_move(move):
     if not (random.randint(1,100) == random.randint(1,100)):
         if move == "rock":
