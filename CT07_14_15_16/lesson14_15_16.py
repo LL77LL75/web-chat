c80582e9c04b3313faaa6a6fe8b1d6626f1dc36a
LL77LL75 <lancelimenze13@gmail.com>
2025-04-28 08:54:34 +0000

    Apr 28, 2025, 8:54 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -5,7 +5,8 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption("Pong Game")
-white = (255,255,255)
+white = ((255,255,255))
+red = ((255,0,0))
 paddle_width = 20
 paddle_height = 100
 paddle1_x = 10
