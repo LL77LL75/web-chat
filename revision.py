d7732b5b793563a699a40049b4a0498f2218e60b
LL77LL75 <lancelimenze13@gmail.com>
2025-05-02 10:08:11 +0000

    May 2, 2025, 10:08 AM

--- revision.py
+++ revision.py
@@ -5,13 +5,13 @@ classes = [
 ]
 
 # Q1 Print out Frank
-print(classes[1][2])
+# print(classes[1][2])
 classes[2][1] = "heather"
 # Q2 Change Hannah to Heather
 
 # Q3 Using list slicing and list concatenation, output the following sublist:
 # ["Asher", "Bob", "Dan", "Esther"]
-print(classes[0][0:2]+classes[1][0:2])
+# print(classes[0][0:2]+classes[1][0:2])
 
 # Q4 Using a for loop, get the list of students in class who are also awardees
 # Define a function flatten(...) that takes a 2d list and returns a 1d list
