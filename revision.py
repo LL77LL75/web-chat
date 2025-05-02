ba40f1e04a7bc38440b9ee5e54e77e3e5c643fa0
LL77LL75 <lancelimenze13@gmail.com>
2025-05-02 10:14:31 +0000

    May 2, 2025, 10:14 AM

--- revision.py
+++ revision.py
@@ -54,6 +54,7 @@ for i in VeryLongList:
 print(min(odd))
 print(max(odd))
 print(sum(odd)/len(odd))
-print
+print(len(odd))
+print()
 # Odd - Smallest: 3, Largest: 91, Mean: 52.375, Length: 48
 # Even - Smallest: 8, Largest: 100, Mean: 49.0, Length: 52
