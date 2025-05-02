a930734e575ba81eb6466b18b960ddd7a746f214
LL77LL75 <lancelimenze13@gmail.com>
2025-05-02 09:36:51 +0000

    May 2, 2025, 9:36 AM

---
+++ revision.py
@@ -0,0 +1,12 @@
+classes = [
+    ["Asher", "Bob", "Charles"],
+    ["Dan", "Esther", "Frank"],
+    ["Gina", "Hannah", "Ian"]
+]
+
+# Q1 Print out Frank
+
+# Q2 Change Hannah to Heather
+
+# Q3 Using list slicing and list concatenation, output the following result:
+# ["Asher", "Bob", "Dan", "Esther"]
