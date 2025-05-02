528ef71172fab816c3302440f221ce4d3c37d251
LL77LL75 <lancelimenze13@gmail.com>
2025-05-02 09:46:54 +0000

    May 2, 2025, 9:46 AM

--- revision.py
+++ revision.py
@@ -11,7 +11,7 @@ classes[2][1] = "heather"
 
 # Q3 Using list slicing and list concatenation, output the following sublist:
 # ["Asher", "Bob", "Dan", "Esther"]
-classes[1][0:]
+(classes[1][0:1])
 # Q4 Using a for loop, get the list of students in class who are also awardees
 # Define a function flatten(...) that takes a 2d list and returns a 1d list
 # Define a function intersect(..., ...) that takes 2 1d lists and returns the common elements in a list
