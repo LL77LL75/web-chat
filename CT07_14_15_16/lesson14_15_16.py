1b371ae2d419432a37e7716e1d133ed6e3b44f3c
LL77LL75 <lancelimenze13@gmail.com>
2025-05-09 10:53:29 +0000

    May 9, 2025, 10:53 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -20,7 +20,8 @@ running = True
 ball_radius = 10
 ball_x = screen_width//2
 ball_y = screen_height//2
-
+ball_x
+ba
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
