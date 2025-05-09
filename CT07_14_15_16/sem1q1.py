642a3e61dac7ef3c932f83222a2bdeefd76e2c99
LL77LL75 <lancelimenze13@gmail.com>
2025-05-09 10:04:33 +0000

    May 9, 2025, 10:04 AM

--- CT07_14_15_16/sem1q1.py
+++ CT07_14_15_16/sem1q1.py
@@ -3,10 +3,7 @@ daily_sales = [1205, 986, 1354, 10535, 15741, 11200, 800,
                1238, 1458, 7803, 900, 13674, 14539, 13241, 
                10886, 7541, 8743, 1482, 11523, 977, 12181, 
                8903, 1008, 1530]
-# Example Output/ Answer:
-# 5 August has highest sales of $15741
-# 7 August has lowest sales of $800
-# Average daily sales for August is $6714.71
+
 maximum = max(daily_sales)
 minimum = min(daily_sales)
 index_num_max = str(daily_sales.index(maximum)+1)
