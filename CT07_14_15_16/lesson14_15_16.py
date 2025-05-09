4551e72c46e76ee16994705ce6c50f41de6d0882
LL77LL75 <lancelimenze13@gmail.com>
2025-05-09 10:45:19 +0000

    May 9, 2025, 10:45 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -21,11 +21,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] and paddle1_y >0:
-            paddle1_y-=5
-        if keys[pygame.K_s] and paddle1_y < screen_height - paddle_height:
-            paddle1_y+=5
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and paddle1_y >0:
+        paddle1_y-=5
+    if keys[pygame.K_s] and paddle1_y < screen_height - paddle_height:
+        paddle1_y+=5
     screen.fill(gray)
     pygame.draw.rect(screen,white,(paddle1_x,paddle1_y,paddle_width,paddle_height))
     pygame.draw.rect(screen,white,(paddle2_x,paddle2_y,paddle_width,paddle_height))
