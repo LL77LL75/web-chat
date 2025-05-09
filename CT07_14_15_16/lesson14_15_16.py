17a86f022973693f1c7f874ad45c6356966364ed
LL77LL75 <lancelimenze13@gmail.com>
2025-05-09 10:50:06 +0000

    May 9, 2025, 10:50 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -38,6 +38,6 @@ while running:
     screen.fill(gray)
     pygame.draw.rect(screen,white,(paddle1_x,paddle1_y,paddle_width,paddle_height))
     pygame.draw.rect(screen,white,(paddle2_x,paddle2_y,paddle_width,paddle_height))
-    pygame.draw.circle()
+    pygame.draw.circle(screen,white)
     pygame.display.flip()
 pygame.quit()
