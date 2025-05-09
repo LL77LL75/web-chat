f7397043518b580dba581ec993ce7eb041508be0
LL77LL75 <lancelimenze13@gmail.com>
2025-05-09 09:51:54 +0000

    May 9, 2025, 9:51 AM

--- CT07_14_15_16/sem1q1.py
+++ CT07_14_15_16/sem1q1.py
@@ -3,4 +3,9 @@ daily_sales = [1205, 986, 1354, 10535, 15741, 11200, 800,
                1238, 1458, 7803, 900, 13674, 14539, 13241, 
                10886, 7541, 8743, 1482, 11523, 977, 12181, 
                8903, 1008, 1530]
-print(str((max(daily_sales))) + " Agust has the best sales of " + str(max(daily_sales)))
+# Example Output/ Answer:
+# 5 August has highest sales of $15741
+# 7 August has lowest sales of $800
+# Average daily sales for August is $6714.71
+
+print(str(daily_sales.index(max(daily_sales))) + " Agust has the best sales of " + str(max(daily_sales)))
