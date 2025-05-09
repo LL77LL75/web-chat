cbc28a35068aaabf92b1e8d72050ad1a1cd8a2f0
LL77LL75 <lancelimenze13@gmail.com>
2025-05-09 10:58:30 +0000

    May 9, 2025, 10:58 AM

--- CT07_14_15_16/lesson14_15_16.py
+++ CT07_14_15_16/lesson14_15_16.py
@@ -44,7 +44,7 @@ while running:
     ball_y += ball_dy
     if ball_y <= 0 or ball_y > screen_height:
         ball_y *= -1
-    if ball_x <= 0 or ball_x > screen_height:
-        ball_x *= -1
+    if ball_x <= 0 or ball_x > screen_width:
+        ball_y *= -1
     pygame.display.flip()
 pygame.quit()
