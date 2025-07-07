1fff2071d202039a0b30b9e32c0ace8ac9117a53
LL77LL75 <lancelimenze13@gmail.com>
2025-07-07 07:39:23 +0000

    wordle2

--- wordle2.py
+++ wordle2.py
@@ -1,7 +1,7 @@
 
 word = ""
-with open("five_letter_words.csv","r") as f:
-    content = f.read
+with open("FiveLetterWords.csv","r") as f:
+    content = f.read()
     words = content.split(",")
 import random
 cWord = random.choice(words)
@@ -11,9 +11,8 @@ def getWord(wordList):
         word = input("your guess>>> ")
 
 
-
-def evaluate_guess(guess,cWord):
-    while
+guess = input("your guess>> ")
+def evaluate_guess(guess,cWord,wordlist):
     eval = ""
     for i in range(5):
         if guess[i] == cWord[i]:
@@ -22,4 +21,21 @@ def evaluate_guess(guess,cWord):
             eval+= "?"
         else:
             eval+="#"
-print("a")
+    print(eval)
+def validate(guess,wordlist):
+    if not guess.isalpha():
+        print("enter a 5-letter word BEEP BEEP BOOP BOOP")
+        return False
+    elif len(guess)!=5:
+        print("enter a five-letter word BEEP BEEP BOOP BOOP")
+        return False
+    elif not guess in wordlist:
+        print("enter a real five-letter word BEEP BEEP BOOP BOOP")
+        return False
+    else:
+        return True
+
+getWord(words)
+for i in range(6):
+    evaluate_guess(guess,cWord,words)
+    guess = input("your guess>>")
