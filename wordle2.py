38865ef1adf56d195e65fec4ac3ba9b2aacd122c
LL77LL75 <lancelimenze13@gmail.com>
2025-07-19 11:24:51 +0000

    ok

--- wordle2.py
+++ wordle2.py
@@ -1,13 +1,16 @@
 import time
 import os
-global font_size
-font_size = input("Enter desired font size for the terminal (e.g., 12, 14, 16): ")
-try:
-    font_size_int = int(font_size)
-    os.system(f'echo -e "\\033]50;SetFont=Monospace {font_size_int}\\007"')
-    os.system("clear")
-except ValueError:
-    print("Invalid font size. Using default.")
+blow_off_list = ["OOPS MOMENT","WOMP WOMP","SKILL ISSUE","SMOOTH DISASTER"]
+win_list = ["Genius move!","Impressive!","You nailed it!","Well played!","Masterful guess!"]
+a = 1
+# global font_size
+# font_size = input("Enter desired font size for the terminal (e.g., 12, 14, 16): ")
+# try:
+#     font_size_int = int(font_size)
+#     os.system(f'echo -e "\\033]50;SetFont=Monospace {font_size_int}\\007"')
+#     os.system("clear")
+# except ValueError:
+#     print("Invalid font size. Using default.")
 
 import random
 
@@ -65,7 +68,7 @@ def validate(user_guess, wordlist):
         print("minus hint or minus hints to remove hints ")
         print("reset hints or reset hint to reset hints ")
         print("set hints or set hint to set hints ")
-        print("font size or fonts or font to change the font size ")
+        # print("font size or fonts or font to change the font size ")
         print("abort to exit commands")
         command = input("Enter a command:")
         if command == "add hint" or command == "add hints":
@@ -102,16 +105,16 @@ def validate(user_guess, wordlist):
                     f.write(set_hints)
                 sync_hints_file()
                 os.system("clear")
-        elif command == "font size" or command == "fonts" or command == "font":
-            code = input("Enter the code: ")
-            font_size = input("Enter desired font size for the terminal (e.g., 12, 14, 16): ")
-            if code in codes:
-                try:
-                    font_size_int = int(font_size)
-                    os.system(f'echo -e "\\033]50;SetFont=Monospace {font_size_int}\\007"')
-                    os.system("clear")
-                except ValueError:
-                    print("Invalid font size. Using default.")
+        # elif command == "font size" or command == "fonts" or command == "font":
+        #     code = input("Enter the code: ")
+        #     font_size = input("Enter desired font size for the terminal (e.g., 12, 14, 16): ")
+        #     if code in codes:
+        #         try:
+        #             font_size_int = int(font_size)
+        #             os.system(f'echo -e "\\033]50;SetFont=Monospace {font_size_int}\\007"')
+        #             os.system("clear")
+        #         except ValueError:
+        #             print("Invalid font size. Using default.")
         elif command == "abort":
             print("Aborting commands.")
             return False
@@ -162,11 +165,11 @@ def get_guess(wordlist):
         elif user_guess == "help":
             print("enter a five-letter word, or type 'restart' to restart the game, or 'quit' to quit.")
         elif user_guess == "hint" and hints > 0:
-            print("the word contains the letter: " + random.choice(cWord))
             hints -= 1
             with open(HINTS_FILE, "w") as f:
                 f.write(str(hints))
             sync_hints_file()
+            print("the word contains the letter: " + random.choice(cWord))
             print("You have " + str(hints) + " hints left.")
         if validate(user_guess, wordlist):
             return user_guess
@@ -174,19 +177,28 @@ def play_game():
     global hints
     global cWord
     global win
+    global a
     win = False
+    a = 1
     cWord = random.choice(words)
+
+print(f"{"#"*30:^30}")
+print(f"{"#"}{"Welcome to Wordle!":^28}{"#"}")
+print(f"{"#"*30:^30}")
+print("You have 6 guesses to find the 5-letter word.")
 while True:
-    for i in range(7):
+    while a <= 6:
         if user_guess == "restart":
             cWord = random.choice(words)
-            i = 0
+            a = 1
+            user_guess = ""
             print("game restarted")
-        print("guess " + str(i+1) + " of 6")
+        print("guess " + str(a) + " of 6")
         user_guess = get_guess(words)
         evaluate_guess(user_guess, cWord)
+        a+=1
         if user_guess == cWord:
-                print("You win!")
+                print("You win! " + random.choice(win_list))
                 win = True
                 hints += 1
                 with open(HINTS_FILE, "w") as f:
@@ -194,8 +206,8 @@ while True:
                 sync_hints_file()
                 print("you have " + str(hints) + " hints now")
                 break
-        if win == False and i == 6:
-            print("You lose! The word was: " + cWord)
+        if win == False and a == 7:
+            print(random.choice(blow_off_list) +" the word was "+ cWord)
     continue_game = input("Do you want to play again? (yes/no): ").lower()
     if continue_game == "yes":
         play_game()
@@ -208,7 +220,7 @@ while True:
     cWord = random.choice(words)
     print("New game started. You have " + str(hints) + " hints.")
     print("you have " + str(hints) + " hints now")
-    if win == False and i == 6:
+    if win == False and a == 6:
         print("You lose! The word was: " + cWord)
     continue_game = input("Do you want to play again? (yes/no): ").lower()
     if continue_game == "yes":
