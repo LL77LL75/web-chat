6bee43c46e7ae8ce90c789dddfb3090b1d579031
LL77LL75 <lancelimenze13@gmail.com>
2025-09-26 10:55:56 +0000

    e

--- stuff/CT07_01/lesson1.py
+++
@@ -1,6 +0,0 @@
-# toppings = ""
-# newTopping = input("what topping ")
-# while newTopping != "end":
-#     toppings += (" " + newTopping)
-#     newTopping= input("what topping ")
-# print(toppings)
