31ae11d3ce458b7dd8cd88f25b1a87ff755ee798
LL77LL75 <lancelimenze13@gmail.com>
2025-10-15 10:23:39 +0000

    deleted

--- frontend/dashboard.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("joinBtn").onclick = () => {
-  const addr = document.getElementById("roomAddr").value.trim();
-  if (!addr) {
-    alert("Enter room address");
-    return;
-  }
-  localStorage.setItem("roomAddr", addr);
-  window.location.href = "room.html";
-};
